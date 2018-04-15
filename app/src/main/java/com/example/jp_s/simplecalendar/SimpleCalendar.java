b2b2618556918da59d6dc87982ce6f1c7bf2f788
Radaeld <joao.serras@ist.utl.pt>
2018-04-15 20:42:30 +0100

    stuff

--- app/src/main/java/com/example/jp_s/simplecalendar/SimpleCalendar.java
+++ app/src/main/java/com/example/jp_s/simplecalendar/SimpleCalendar.java
@@ -68,8 +68,8 @@ public class SimpleCalendar{
             }
         });
 
-        globalview.findViewById(R.id.back_action).setOnClickListener(v-> swipeListener("Left"));
-        globalview.findViewById(R.id.front_action).setOnClickListener(v-> swipeListener("Right"));
+        globalview.findViewById(R.id.back_action).setOnClickListener(v-> swipeListener("Right"));
+        globalview.findViewById(R.id.front_action).setOnClickListener(v-> swipeListener("Left"));
 
         //globalview.setOnTouchListener(swipeListener());
         selectDay(days[todayDay+(currentDayOfWeek - 1)]);
